a5b3ec6f66d7e04e959a1a40bb150989e422fbe1
Manuel Banz <mbnz123@gmail.com>
2012-07-08 08:38:08 -0400

    new test files

--- nodejstest/app.js
+++ nodejstest/app.js
@@ -3,7 +3,7 @@
  * Module dependencies.
  */
 
-var express = require('express'), 
+var express = require('express'),
     routes = require('./routes');
 
 var app = module.exports = express.createServer();
@@ -32,6 +32,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
